8e335e2347a39036939c0138543b8c8c99a7b5e4
Mark Waite <mark.earl.waite@gmail.com>
2018-05-20 23:34:02 -0600

    Format source files

--- sample_maven/src/main/java/com/markwaite/App.java
+++ sample_maven/src/main/java/com/markwaite/App.java
@@ -1,13 +1,12 @@
 package com.markwaite;
 
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
